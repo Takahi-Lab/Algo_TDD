07322b02330bdddcf25751c317acd99d6deba0ca
belamar1 <belamar75@gmail.com>
2023-01-01 22:03:57 +0000

    Tested and refactored

--- node-es6/src/__test__/task3.test.js
+++ node-es6/src/__test__/task3.test.js
@@ -1,7 +1,28 @@
 const task3 = require('../task3')
 
 describe('task 3 solver', () => {
-  it('should return 13', () => {
-    expect(task3.solve()).toEqual(13)
+  
+  it('should return "" if n is not divisible by 2 or 3', () => {
+    expect(task3.solve(1)).toEqual("")
+    expect(task3.solve(7)).toEqual("")
+    expect(task3.solve(11)).toEqual("")
   })
+    it('should return "fizz" if n is not divisible by 2 but not 3', () => {
+      expect(task3.solve(2)).toEqual("fizz")
+      expect(task3.solve(4)).toEqual("fizz")
+      expect(task3.solve(8)).toEqual("fizz")
+
+  })
+    it('should return "buzz" if n is not divisible by 3 but not 2', () => {
+      expect(task3.solve(3)).toEqual("buzz")
+      expect(task3.solve(9)).toEqual("buzz")
+      expect(task3.solve(15)).toEqual("buzz")
+})
+    it('should return "fizzbuzz" if n is divisible by 2 and 3', () => {
+      expect(task3.solve(2*3)).toEqual("fizzbuzz")
+      expect(task3.solve(12)).toEqual("fizzbuzz")
+      expect(task3.solve(2*3*7)).toEqual("fizzbuzz")
+
+    })
+
 })
--- node-es6/src/task3.js
+++ node-es6/src/task3.js
@@ -1,6 +1,11 @@
 
-function solve() {
-  return 13
+function solve(n) {
+  var words = []
+  if (n % 2 === 0)
+  words.push("fizz")
+  if (n % 3 === 0)
+  words.push("buzz")
+  return words.join("")
 }
 
 module.exports.solve = solve
